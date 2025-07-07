7082047475ad40437a4bb142f775d88838532e4f
Aromalvg <aromalvijayan448@gmail.com>
2025-07-07 15:31:54 +0530

    whtasapp

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -4,7 +4,7 @@ export default function WhatsAppButton() {
   return (
     <div className="fixed bottom-6 right-6 z-50 group">
       <button
-        className="bg-green-500 hover:bg-green-600 p-4 rounded-full shadow-xl text-white transition-all duration-300"
+        className="bg-green-500 hover:bg-green-600 p-3 rounded-full shadow-xl text-white transition-all duration-300"
         onClick={() => window.open('https://wa.me/7592866028', '_blank')}
         aria-label="WhatsApp"
       >
